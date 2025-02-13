96aa5746cd79cdae57267691780772c91a899ebf
Peer Faizan <124279575+Faizan-313@users.noreply.github.com>
2025-02-13 17:43:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if not os.path.exists(model_path):
     gdown.download(url,model_path,quiet=False)
 
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    model = tf.keras.models.load_model(model_path)
 
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128)) 
     input_arr = tf.keras.preprocessing.image.img_to_array(image) 
